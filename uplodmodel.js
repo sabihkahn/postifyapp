cb464a30820d595f13e37833df0f0d670dee135e
sabihkahn <whyonlyhead@gmail.com>
2025-06-25 13:35:07 +0500

    uploding code

---
+++ uplodmodel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const postschema = new mongoose.Schema({
+  photo: {
+    name: String,
+    data: Buffer,
+    contentType: String,
+  },
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  comments: [
+    {
+      text: String,
+      createdAt: { type: Date, default: Date.now }
+    }
+  ],
+  followers: {
+       type: Number,
+       default: 0
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  creator: String,
+
+});
+
+export default mongoose.model('postdata', postschema);
